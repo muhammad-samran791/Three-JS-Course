44efdb84edc1a0bc9dccd3cfd13e2266827a413a
Webcraft Innovation <135718962+samranrajput@users.noreply.github.com>
2026-06-19 15:42:31 +0500

    Three JS Course

---
+++ 01-Intro/index.js
@@ -0,0 +1,22 @@
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(20, 20, 20);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const box = new THREE.Mesh(geometry, material);
+scene.add(box);
+
+const size = {
+  width: 700,
+  height: 500,
+};
+
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
+camera.position.z = 50;
+camera.position.x = 20;
+scene.add(camera);
+
+const target = document.getElementById("target");
+const renderer = new THREE.WebGLRenderer({ canvas: target });
+
+renderer.setSize(size.width, size.height);
+renderer.render(scene, camera);
